ad7ae9ddffba5fd8af95f370436e2fae0569fbb8
Tosin Bamigboye <tob103@yahoo.com>
2022-09-18 04:26:39 -0500

    Added More Css and set up limited Routing

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
 import React from "react";
-import CategoryDirectory from "./Components/CategoryDirectory/CategoryDirectory";
+import Home from "./Components/Routes/Home/Home";
+import NavigationBar from "./Components/NavigationBar/NavigationBar";
+import {Route, Routes} from "react-router";
+
+const Shop = () => {
+    return (
+        <h1> I am the Shop Component </h1>
+    )
+};
 
 const App = () => {
-
-    const categories = [
-        {
-            "id": 1,
-            "title": "hats",
-            "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-        },
-        {
-            "id": 2,
-            "title": "jackets",
-            "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-        },
-        {
-            "id": 3,
-            "title": "sneakers",
-            "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-        },
-        {
-            "id": 4,
-            "title": "womens",
-            "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-        },
-        {
-            "id": 5,
-            "title": "mens",
-            "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-        }
-    ]
-
-
     return (
-         <div>
-            <CategoryDirectory categoryArray={categories} />
-         </div>
-
+        <Routes>
+            <Route path='/' element={<NavigationBar />}>
+                       <Route index element={<Home/>} />
+                <Route path="shop" element={<Shop/>}/>
+            </Route>
+        </Routes>
     );
 };
 
---
+++ src/Components/NavigationBar/NavigationBar.js
@@ -0,0 +1,26 @@
+import React, {Fragment} from "react";
+import {Outlet} from "react-router";
+import {Link} from "react-router-dom";
+import {ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+import './NavigationBar.styles.scss'
+
+const NavigationBar = () => {
+    return (
+        <Fragment>
+            <div className='navigation'>
+                {/*<h1> I am the navigation bar </h1>*/}
+                <Link className='logo-container' to='/'>
+                   <CrwnLogo className='logo'/>
+                </Link>
+                <div className="nav-links-container">
+                    <Link className='nav-link' to='/shop'>
+                        SHOP
+                    </Link>
+                </div>
+            </div>
+            <Outlet/>
+        </Fragment>
+    )
+}
+
+export default NavigationBar;
